a6b097ec8628a8c6cd9f06f3af2644452b709b5c
Davide Botti <davidebotti92@gmail.com>
2024-12-22 09:35:01 +0100

    New names for the containers

--- run-local-environment.sh
+++ run-local-environment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#docker run --name some-redis --rm -p 6379:6379 -d redis
+#docker run --name redis --rm -p 6379:6379 -d redis
 
-docker run --name my-memcache --rm -p 11211:11211 -d memcached
+docker run --name memcache --rm -p 11211:11211 -d memcached
